7076bb46ad260bfad929a9e96a9bd4c6468d5a54
Cerul Alain <ae@metaeducation.com>
2026-07-10 04:58:52 -0400

    Qualify uses of dont() in the needful headers
    Shorthands may not be in effect.

--- needful-known.hpp
+++ needful-known.hpp
@@ -409,7 +409,7 @@ struct ExactWrapper {
     ExactWrapper(const SinkWrapper<U*>& sink)
         : p {static_cast<TP>(sink)}
     {
-        dont(NEEDFUL_ASSERT(not sink.corruption_pending));  // must allow corrupt [2]
+        needful_dont(NEEDFUL_ASSERT(not sink.corruption_pending));  // [2]
     }
 
     template<typename U, IfExactType<U>* = nullptr>
@@ -442,7 +442,7 @@ struct ExactWrapper {
 
     template<typename U, IfExactType<U>* = nullptr>
     ExactWrapper& operator=(const SinkWrapper<U*>& sink) {
-        dont(NEEDFUL_ASSERT(not sink.corruption_pending));  // must allow corrupt [2]
+        needful_dont(NEEDFUL_ASSERT(not sink.corruption_pending));  // [2]
         this->p = static_cast<TP>(sink);  // not sink.p (flush corruption)
         return *this;
     }
